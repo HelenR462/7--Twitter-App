f6135ca2f48bbdf3a4a1b3eef19c6bf986b3c7f2
Mimi397 <izzena@hotmail.com>
2023-07-28 21:37:38 -0400

    Both servers are up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
+import { useState, useEffect } from "react";
 function App() {
+  const [name, setName] = useState("Unknown");
+
+  useEffect(() => {
+    // Make an http request to our server for the name
+    axios.get("/api/name").then((response) => {
+      // update 'name' to be the name from our server
+      setName(response.data.name);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{name} is the best!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/name", (req, res) => {
+  const response={
+    name:"Andy",
+  }
+
+  res.send(response)
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
