88318904f6b87ae751ec7af49fb4a330800a16f0
Helen <izzena@hotmail.com>
2024-02-06 17:29:26 -0500

    updated card image anddate time stamp

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,25 +2,33 @@ import React from "react";
 import "./Card.css";
 import Footer from "../Footer";
 
-function Card({ selectedUser, randomTweet }) {
+function formatDate(timestamp) {
+  const date = new Date(timestamp);
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  return date.toLocaleString("en-US", options);
+}
+
+function Card({ selectedUser, randomTweet, imgSrc }) {
   return (
     <div className="card-container">
-      <img
-        className="card-avatar-img"
-        src="./images/icons8-user-avatar-50.png"
-        alt=""
-      />
+      <img className="card-avatar-img" src={imgSrc} alt="" />
       {selectedUser && (
         <div>
           <h4>{selectedUser.name}</h4>
 
           {/* <h5>{selectedUser.screen_name}</h5> */}
-          
 
           {randomTweet && (
             <div className="tweet-container">
-              <p>{randomTweet.created_at}</p>
-           <p>{randomTweet.text}</p>
+              <p>{formatDate(randomTweet.created_at)}</p>
+              <p>{randomTweet.text}</p>
             </div>
           )}
         </div>
